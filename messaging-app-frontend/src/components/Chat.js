8e1d2579fd313639723073bc3dc9d37d7f903d60
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-10-30 18:50:02 +0100

    Added the Chat footer component.

--- messaging-app-frontend/src/components/Chat.js
+++ messaging-app-frontend/src/components/Chat.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
+import { AttachFile, MoreVert, SearchOutlined, InsertEmoticon } from '@material-ui/icons'
+import MicIcon from '@material-ui/icons/Mic'
+
 import './Chat.css'
 const Chat = () => {
 
@@ -55,7 +57,17 @@ const Chat = () => {
                     </span>
                 </p>
             </div>
-            <div className="chat__footer"></div>
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input
+                        placeholder="Type a message"
+                        type="text"
+                    />
+                    <button type="submit">Send a message</button>
+                </form>
+                <MicIcon />
+            </div>
         </div>
     )
 }

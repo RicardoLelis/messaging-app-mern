f7322933aed07360353db1ac02ec55694d724806
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-10-30 21:04:20 +0100

    Added the Frontend changes to enable Pusher realtime messaging.

--- messaging-app-frontend/src/App.js
+++ messaging-app-frontend/src/App.js
@@ -1,7 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import Pusher from 'pusher-js'
+
 import './App.css';
 import Sidebar from './components/Sidebar'
 import Chat from './components/Chat'
+
 function App() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    const pusher = new Pusher('6e9871ac51ee4c39104a', {
+      cluster: 'eu'
+    })
+    const channel = pusher.subscribe('messages')
+    channel.bind('inserted', (data) => {
+      setMessages([...messages, data])
+    })
+    return () => {
+      channel.unbind_all()
+      channel.unsubscribe()
+    }
+  }, [messages])
+  console.log(messages)
+
   return (
     <div className="app">
       <div className="app__body">
